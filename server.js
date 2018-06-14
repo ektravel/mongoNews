563bf9869a704d4407d315af6d6b5f6411be224f
ektravel <38017980+ektravel@users.noreply.github.com>
2018-06-13 23:02:01 -0500

    added scraping and routing to server.js

---
+++ server.js
@@ -0,0 +1,66 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var request = require("request");
+
+//Scraping tools
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+//Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+//Initialize express
+var app = express();
+
+//Configure middleware
+
+//use morgan logger for logging requests
+app.use(logger("dev"));
+
+//use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({extended: true}));
+
+//use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
+//Connect to the Mongo DB
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoNews";
+
+//Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+// mongoose.connect("mongodb://localhost/mongoNews");
+
+//Routes
+
+//A GET route for scraping the onion website 
+app.get("/scrape", function(req, res){
+    //grab the body of the html with request
+    axios.get("https://www.theonion.com/").then(function(response){
+        //load the data into cheerio and save it to $ for a shorthand selector
+        var $ = cheerio.load(response.data);
+        //grab every title arh1 within a header tag
+        $("header h1").each(function(i, element){
+            //save an empty result object
+            var result = {};
+            //add the text and href of every link, and save them as properties of the result object
+            result.title = $(this).children("a").text();
+            result.link = $(this).children("a").attr("href");
+            //create a new Article using the "result" object built from scraping
+            db.Article.create(result)
+            .then(function(dbArticle){
+                //view the added result in the console
+                console.log(dbArticle);
+            }).catch(function(err){
+                //if an error occured, send it to the client
+                return res.json(err);
+            });
+        });
+        //if an Article was scraped and saved successfully, display this message
+        res.send("Scrape Complete");
+    });
+});
